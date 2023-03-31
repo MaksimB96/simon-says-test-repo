1776bedf37c7ba9fdb3a491fc2080290134fff15
MaksimB96 <maksims.buraks.1996@gmail.com>
2023-03-31 14:38:15 +0000

    Add additional tests

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,8 @@
  * @jest-environment jsdom
  */
 
-const {game} = require("../game");
+
+const {game, newGame} = require("../game");
 
 beforeAll(() => {
     let fs = require('fs');
@@ -24,5 +25,30 @@ describe("game objct contains correct keys", () => {
     });
     test("choices key exists", () => {
         expect("choices" in game).toBe(true);
+    });
+    test("choices contain correct ids", () => {
+        expect(game.choices).toEqual(["button1","button2","button3","button4"]);
+    });
+});
+
+describe("newGame works correctly", () => {
+    beforeAll(() => {
+        game.score = 42;
+        game.playerMoves = ["button1","button2"];
+        game.currentGame = ["button1","button2"];
+        document.getElementById("score").innerText = "42";
+        newGame();
+    });
+    test("should set game scor to 0", () => {
+        expect(game.score).toEqual(0);
+    });
+    test("should clear computer sequence array", () => {
+        expect(game.currentGame.length).toBe(0);
+    });
+    test("should clear player moves array", () => {
+        expect(game.playerMoves.length).toBe(0);
+    });
+    test("should display 0 for ID score", () => {
+        expect(document.getElementById("score").innerText).toEqual(0);
     });
 });
